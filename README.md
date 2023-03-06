391b2c467a3517f83a9e5055e67576317207e34a
ayyoubmaulana <ayyoub@hostinger.com>
2023-03-06 21:23:21 +0700

    ml: Add docker

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 1. Install dependencies using `pip install -r requirements.txt`
 2. Run `python3 models/regression.py` to run and test the model
 3. Run `python3 server/regression.py` to run endpoint server reggresion
+
+# Run with docker
+1. `docker-compose up --build -d`
--- models/regression.py
+++ models/regression.py
@@ -4,18 +4,25 @@ from sklearn.linear_model import LinearRegression
 import pickle
 import os
 
+# Get path to file dynamically
 WD = os.path.dirname(os.path.abspath(__file__))
 
+# read data source
 dataset = pd.read_csv('data/salary.csv')
+
+# define x (independent) and y (dependent) variable
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
+# Split data to train (67%) and test (33%)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 0)
 
+# Develop regression model
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 
+# save model to pickle
 pickle.dump(regressor, open(WD + '/pickled/model.pkl','wb'))
 
 
